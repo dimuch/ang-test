6f36fcdb29972b9bf0f6a26c19c5cfa23b715849
dmytro <dmytro@ofload.com>
2024-07-04 21:45:27 +0200

    Empty component

---
+++ src/app/password-strength-indicator/password-strength-indicator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-strength-indicator',
+  standalone: true,
+  imports: [],
+  templateUrl: './password-strength-indicator.component.html',
+  styleUrl: './password-strength-indicator.component.css'
+})
+export class PasswordStrengthIndicatorComponent {
+
+}
